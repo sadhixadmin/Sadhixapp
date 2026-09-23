6a4a882d548fa231bcd9a8a3cf2ac9a121db5bec
Sadhix <moizhussain89569@gmail.com>
2026-09-23 21:57:46 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const PORT = 3000; // You can change the port if needed
 
 // Establishing a connection with the database
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '172.31.6.215',
   user: 'root',
   password: 'Password@1234',
   database: 'sadhixdb'
